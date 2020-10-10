6735905a8ee60d2f4ceca44d147cf8726398c31b
Dana Jackson <danajackson2@gmail.com>
2020-10-10 17:48:21 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,5 +1,6 @@
-count = 0
-while count < 3 do
-  puts "I am the #{count}, I love to count!"
-  count = count + 1
+count = 10
+while count > 0 do
+  puts count
+  count -= 1
 end
+puts "Happy New Year!"
